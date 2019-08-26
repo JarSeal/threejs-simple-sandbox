85dbf701459950b44d8fb9f33155e16a3fa689e9
JarSeal <kai.forsman@gmail.com>
2019-08-26 22:32:19 +0300

    Merge ship and modules together in load-map.js.

--- src/js/data/dev-ship.js
+++ src/js/data/dev-ship.js
@@ -2,12 +2,14 @@
 // A ship for development purposes
 
 export function getShip() {
+    let floors = getShipFloors();
     return Object.assign(
         {},
         getShipGlobalAttrs(),
         {
             pos: [2,2],
-            floors: getShipFloors()
+            dims: [floors[0].tileMap[0].length, floors[0].tileMap.length],
+            floors: floors
         }
     );
 };
@@ -22,15 +24,26 @@ function getShipFloors() {
     return [
         {
             floor: 1,
+            // tileMap: [ // module's first number is the type of module and second is a running index of all the modules in the current map (for cases where there are multiple same type of modules on the same map)
+            //     [{module:[1,0],level:1,pos:[2,2],dims:[8,8]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
+            //     [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}]
+            // ],
             tileMap: [ // module's first number is the type of module and second is a running index of all the modules in the current map (for cases where there are multiple same type of modules on the same map)
-                [{module:[1,0],level:1,pos:[2,2],dims:[8,8]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}],
-                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]}]
+                [{module:[1,0],level:1,pos:[2,2],dims:[8,8]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1],level:1,pos:[10,2],dims:[8,8]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,0]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]},{module:[1,1]}],
+                [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}]
             ],
         },
     ]
--- src/js/tilemap/load-map.js
+++ src/js/tilemap/load-map.js
@@ -3,30 +3,67 @@ import { getModule } from './modules/index.js';
 
 class LoadTileMap {
     constructor(mtlLoader, objLoader, scene) {
+        this.ship = [];
         this.init(mtlLoader, objLoader, scene);
     }
 
     init(mtlLoader, objLoader, scene) {
-        new Promise((resolve) => {
-            mtlLoader.load('test-floor2.mtl', (materials) => {
-                resolve(materials);
-            })
-        })
-        .then((materials) => {
-            materials.preload();
-            objLoader.setMaterials(materials);
-            objLoader.load('test-floor2.obj', (object) => {
-                object.rotation.x = 1.5708;
-                object.position.x = 31.5;
-                object.position.y = 31.5;
-                scene.add(object);
-            })
-        });
+        let x, y,
+            modules = [],
+            rawShip = getShip(),
+            xLength = rawShip.dims[0],
+            yLength = rawShip.dims[1],
+            tile,
+            curModuleIndexes;
+        this.ship = rawShip;
+        // Add modules to main map
+        for(y=0;y<yLength;y++) {
+            for(x=0;x<xLength;x++) {
+                tile = rawShip.floors[0].tileMap[y][x];
+                if(tile.level) {
+                    modules.push(getModule(tile.module[0], tile.level));
+                    modules[tile.module[1]].curIndexes = [0,0];
+                }
+                if(tile.module) {
+                    curModuleIndexes = modules[tile.module[1]].curIndexes;
+                    this.ship.floors[0].tileMap[y][x] = Object.assign(
+                        {},
+                        tile,
+                        modules[tile.module[1]].tilemap[curModuleIndexes[0]][curModuleIndexes[1]]
+                    );
+                    if(curModuleIndexes[1] === (modules[tile.module[1]].dims[1] - 1)) {
+                        modules[tile.module[1]].curIndexes[0] += 1;
+                        modules[tile.module[1]].curIndexes[1] = 0;
+                    } else {
+                        modules[tile.module[1]].curIndexes[1] += 1;
+                    }
+                } else {
+                    this.ship.floors[0].tileMap[y][x] = {type:0};
+                }
+            }
+        }
+        //console.log(modules);
+        console.log("this.ship",this.ship);
+        // new Promise((resolve) => {
+        //     mtlLoader.load('test-floor2.mtl', (materials) => {
+        //         resolve(materials);
+        //     })
+        // })
+        // .then((materials) => {
+        //     materials.preload();
+        //     objLoader.setMaterials(materials);
+        //     objLoader.load('test-floor2.obj', (object) => {
+        //         object.rotation.x = 1.5708;
+        //         object.position.x = 31.5;
+        //         object.position.y = 31.5;
+        //         scene.add(object);
+        //     })
+        // });
     }
 
     getCurrentMap() {
         // return getShip();
-        return getModule(1,1);
+        //return getModule(1,1);
     }
 }
 
