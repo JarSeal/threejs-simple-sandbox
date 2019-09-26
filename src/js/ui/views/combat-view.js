4ed9d0e0397a0ad1be4db39e08f41008666febe8
JarSeal <kai.forsman@gmail.com>
2019-09-26 20:46:09 +0300

    Combat view into its own file.

---
+++ src/js/ui/views/combat-view.js
@@ -0,0 +1,85 @@
+
+
+class CombatView {
+    constructor(sceneState) {
+        this.sceneState = sceneState;
+    }
+
+    getView() {
+        if(this.sceneState) {
+            let uiData = [
+                {
+                    type: 'circleButton',
+                    index: 0,
+                    id: 'shootButton',
+                    pos: [75, window.innerHeight - 75],
+                    radius: 50,
+                    keepUpdatingWhenPressed: true,
+                    firstClick: null,
+                    colors: ['rgba(255,255,255,0.5)', 'rgba(255,255,255,1)'],
+                    colorPhase: 0,
+                    color: function(sceneState) {
+                        // UI layer change:
+                        if(this.id == sceneState.ui.curId) {
+                            if(sceneState.ui.curState == 'startClick') {
+                                if(sceneState.ui.viewData[this.index].colorPhase === 0) {
+                                    new TimelineMax().to(sceneState.ui.viewData[this.index], 0.1, {radius:60})
+                                                     .to(sceneState.ui.viewData[this.index], 0.4, {radius:50, ease: Elastic.easeOut});
+                                    sceneState.ui.viewData[this.index].colorPhase = 1;
+                                }
+                                return this.colors[1];
+                            }
+                        }
+                        if(sceneState.ui.viewData[this.index].colorPhase == 1) {
+                            new TimelineMax().to(sceneState.ui.viewData[this.index], 0.1, {radius:40})
+                                             .to(sceneState.ui.viewData[this.index], 0.4, {radius:50, ease: Elastic.easeOut});
+                            sceneState.ui.viewData[this.index].colorPhase = 0;
+                        }
+                        return this.colors[0];
+                    },
+                    actionPhase: 0,
+                    action: function(sceneState, calculateAngle) {
+                        // 3D layer change:
+                        let hero = sceneState.players.hero,
+                            heroMaterial = hero.mesh.children[0].material,
+                            angle;
+                        if(this.id == sceneState.ui.curId && sceneState.ui.curState == 'startClick') {
+                            if(sceneState.ui.viewData[this.index].actionPhase === 0) {
+                                heroMaterial.color.setHex(0xffffff);
+                                sceneState.ui.viewData[this.index].actionPhase = 1;
+                            }
+                            if(sceneState.ui.curSecondaryTarget) {
+                                // Calculate angle for player to turn to
+                                let angle = calculateAngle(hero.pos,sceneState.ui.curSecondaryTarget);
+                                // prevent unnecessary spin moves :)
+                                if(Math.abs(hero.mesh.rotation.z - angle) > Math.PI) {
+                                    angle < 0 ? hero.mesh.rotation.z = hero.mesh.rotation.z + Math.PI * -2 :
+                                                hero.mesh.rotation.z = hero.mesh.rotation.z + Math.PI * 2;
+                                }
+                                new TimelineMax().to(
+                                    hero.mesh.rotation,
+                                    0.2,
+                                    {
+                                        z:angle,
+                                        ease: Sine.easeInOut,
+                                        onComplete: () => {
+                                            hero.dir = angle;
+                                        }
+                                    });
+                                sceneState.ui.curSecondaryTarget = null;
+                            }
+                            return;
+                        }
+                        if(sceneState.ui.viewData[this.index].actionPhase == 1) {
+                            heroMaterial.color.setHex(0xff0088);
+                            sceneState.ui.viewData[this.index].actionPhase = 0;
+                        }
+                    },
+                },
+            ];
+            return uiData;
+        }
+    }
+}
+
+export default CombatView
