771afb1883a572d95f01339c797c1e7f9b98bc26
JarSeal <kai.forsman@gmail.com>
2020-09-15 11:56:17 +0300

    Started code optimisation and linting corrections.

--- src/js/util.js
+++ src/js/util.js
@@ -1,12 +1,14 @@
 
-export function calculateAngle(startPos, endPos) {
+export const calculateAngle = (startPos, endPos) => {
     return Math.atan2(endPos[0] - startPos[0], startPos[1] - endPos[1]);
-}
+};
 
-export function randomInt(from, to) {
+export const randomInt = (from, to) => {
     return Math.floor(Math.random() * (to - from + 1) + from);
-}
+};
 
 export function randomTimeNow(from, to) {
     return performance.now() + randomInt(from, to);
 }
+
+export default { calculateAngle, randomInt, randomTimeNow };
